463c4f648ed5440ed834d44aedf6333147654d6b
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-01-06 10:43:54 -0300

    [+] Estilo (ainda falta arrumar os botões)

--- src.js
+++ src.js
@@ -1,4 +1,8 @@
-function rangeSlide(value) {
-    document.getElementById('rangeValue').innerHTML = value;
+function rangeSlide(valueSlider) {
+    const valor250Tc = 52
+    const valor25Tc = 6
+    const moneyValue = valor250Tc * Math.floor(valueSlider / 250) + valor25Tc * Math.floor((valueSlider % 250) / 25)
+    document.getElementById('rangeValue').innerHTML = `${valueSlider} TC`;
+    document.getElementById('moneyValue').innerHTML = `R$ ${moneyValue},00`;
     console.log('DragEvent')
 }
