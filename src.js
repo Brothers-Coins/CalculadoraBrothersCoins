e8dc87e84d08ef0e51fdc1f93f6e85829b982764
Pierre Vieira <49538805+PierreVieira@users.noreply.github.com>
2021-01-06 11:20:06 -0300

    [+] Click Listeners on buttons

--- src.js
+++ src.js
@@ -1,8 +1,41 @@
-function rangeSlide(valueSlider) {
-    const valor250Tc = 52
-    const valor25Tc = 6
-    const moneyValue = valor250Tc * Math.floor(valueSlider / 250) + valor25Tc * Math.floor((valueSlider % 250) / 25)
-    document.getElementById('rangeValue').innerHTML = `${valueSlider} TC`;
-    document.getElementById('moneyValue').innerHTML = `R$ ${moneyValue},00`;
-    console.log('DragEvent')
+const valor250Tc = 52
+const valor25Tc = 6
+
+function getValueSlider() {
+    return parseInt(document.getElementById('slider').value, 10)
+}
+
+function getMoneyValue(value) {
+    return valor250Tc * Math.floor(value / 250) + valor25Tc * Math.floor((value % 250) / 25)
+}
+
+function setTcValue(valueSlider) {
+    document.getElementById('rangeValue').innerHTML = `${valueSlider} TC`
+    document.getElementById('slider').value = valueSlider
+}
+
+function setMoneyValue(moneyValue) {
+    document.getElementById('moneyValue').innerHTML = `R$ ${moneyValue},00`
+}
+
+function setFields(value) {
+    const moneyValue = getMoneyValue(value)
+    setTcValue(value);
+    setMoneyValue(moneyValue);
+    console.log(`Tc value: ${value}\nMoney value: ${moneyValue}`)
+}
+
+function rangeSlide() {
+    const valueSlider = getValueSlider()
+    setFields(valueSlider);
+}
+
+function buttonLeftClick() {
+    const value = getValueSlider() - 25
+    setFields(value)
+}
+
+function buttonRightClick() {
+    const value = getValueSlider() + 25
+    setFields(value)
 }
